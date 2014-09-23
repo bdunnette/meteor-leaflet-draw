468f012e1f5415a5928d389d8b4b188a50304329
Brian Dunnette <dunn0172@umn.edu>
2014-09-23 15:05:24 -0500

    restructuring

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
   summary: "Adds support for drawing and editing vectors and markers on Leaflet maps",
-  version: "0.2.3",
+  version: "1.0.0",
   git: "https://github.com/bdunnette/meteor-leaflet-draw.git"
 });
 
 Package.onUse(function (api) {
-  api.addFiles('leaflet.draw.js');
-  api.addFiles('leaflet.draw.css');
-  api.addFiles('images/spritesheet.png');
-  api.addFiles('images/spritesheet-2x.png');
+  api.use('leaflet', 'client');
+  api.addFiles('lib/leaflet.draw.js', 'client');
+  api.addFiles('styles/leaflet.draw.css', 'client');
+  api.addFiles('images/spritesheet.png', 'client');
+  api.addFiles('images/spritesheet-2x.png', 'client');
 });
