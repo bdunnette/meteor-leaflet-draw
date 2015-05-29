5cff4e2e1b001768c3d17df52d82117b4afa9151
Brian Dunnette <brian@dunnette.us>
2015-05-29 09:28:24 -0500

    updating to leaflet-draw 0.2.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "bdunnette:leaflet-draw",
     summary: "Adds support for drawing and editing vectors and markers on Leaflet maps",
-    version: "1.0.2",
+    version: "1.0.3",
     git: "https://github.com/bdunnette/meteor-leaflet-draw.git"
 });
 
